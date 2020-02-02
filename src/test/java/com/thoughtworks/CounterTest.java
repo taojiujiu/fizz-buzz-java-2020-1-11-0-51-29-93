8825451ebf029ad98bf455f7d7c2c64f0f78e961
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-02 21:08:39 +0800

    Add more unit test cover multiple 3 5 7 and contain 3 5 7 cases

--- src/test/java/com/thoughtworks/CounterTest.java
+++ src/test/java/com/thoughtworks/CounterTest.java
@@ -15,41 +15,72 @@ public class CounterTest {
 
     @Test
     public void should_return_1_given_number_1() {
-        assertEquals(counter.fizzBuzzWhizz(1),"1");
+        assertEquals("1", counter.fizzBuzzWhizz(1));
     }
 
     @Test
     public void should_return_Fizz_given_number_contain_7_and_multiple_of_3_and_5() {
-        assertEquals(counter.fizzBuzzWhizz(75),"Fizz");
+        assertEquals("Fizz", counter.fizzBuzzWhizz(75));
     }
 
     @Test
-    public void should_return_Fizz_given_number_contain_7_and_multiple_of_3(){
-        assertEquals(counter.fizzBuzzWhizz(72),"Fizz");
+    public void should_return_Fizz_given_number_contain_7_and_multiple_of_3() {
+        assertEquals("Fizz", counter.fizzBuzzWhizz(72));
     }
 
     @Test
-    public void should_return_Whizz_given_number_contain_7_and_multiple_of_7(){
-        assertEquals(counter.fizzBuzzWhizz(77),"Whizz");
+    public void should_return_Whizz_given_number_contain_7_and_multiple_of_7() {
+        assertEquals("Whizz", counter.fizzBuzzWhizz(77));
     }
 
     @Test
-    public void should_return_Whizz_given_number_contain_7_and_multiple_of_7_and_5(){
-        assertEquals(counter.fizzBuzzWhizz(70),"Whizz");
+    public void should_return_Whizz_given_number_contain_7_and_multiple_of_7_and_5() {
+        assertEquals("Whizz", counter.fizzBuzzWhizz(70));
     }
 
     @Test
-    public void should_return_BuzzWhizz_given_number_contain_5_and_multiple_of_3_and_7(){
-        assertEquals(counter.fizzBuzzWhizz(35),"BuzzWhizz");
+    public void should_return_BuzzWhizz_given_number_contain_5_and_multiple_of_3_and_7() {
+        assertEquals("BuzzWhizz", counter.fizzBuzzWhizz(35));
     }
 
     @Test
-    public void should_return_Fizz_given_number_only_contain_3_and_multiple_of_5(){
-        assertEquals(counter.fizzBuzzWhizz(30),"Fizz");
+    public void should_return_Fizz_given_number_only_contain_3_and_multiple_of_5() {
+        assertEquals("Fizz", counter.fizzBuzzWhizz(30));
     }
 
     @Test
-    public void should_return_Fizz_given_number_only_contain_3(){
-        assertEquals(counter.fizzBuzzWhizz(31),"Fizz");
+    public void should_return_Fizz_given_number_only_contain_3() {
+        assertEquals("Fizz", counter.fizzBuzzWhizz(31));
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_multiple_of_3() {
+        assertEquals("Fizz", counter.fizzBuzzWhizz(6));
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_multiple_of_5() {
+        assertEquals("Buzz", counter.fizzBuzzWhizz(10));
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_multiple_of_7() {
+        assertEquals("Whizz", counter.fizzBuzzWhizz(14));
+    }
+
+
+    @Test
+    public void should_return_Fizz_given_number_contain_3() {
+        assertEquals("Fizz", counter.fizzBuzzWhizz(31));
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_contain_5() {
+        assertEquals("51", counter.fizzBuzzWhizz(51));
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_contain_7() {
+        assertEquals("71", counter.fizzBuzzWhizz(71));
     }
 }
